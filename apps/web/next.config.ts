299d18af4c7a34bbb8357533d38f9a41c6deed3f
APTechSolucoesTI <sistema@aptechinfo.com.br>
2026-08-17 13:59:11 -0300

    Update next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   poweredByHeader: false,
+
+  async rewrites() {
+    return [
+      {
+        source: "/backend/:path*",
+        destination: "https://apiapbling.aptechinfo.com.br:75/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
